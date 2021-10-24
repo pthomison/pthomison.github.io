813bd5a90bc77721fe206aa85c74454a3a8ecc2d
p.thomison <p.thomison@gmail.com>
2021-10-24 14:09:54 -0700

    path

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname),
 	},
 	// mode: 'development',
 	mode: 'production',
