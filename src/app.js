de48b4e98e720637582694468bf4f2420b2f45dd
pthomison <p.thomison@gmail.com>
2022-09-06 10:58:09 -0700

    semicolon

--- src/app.js
+++ src/app.js
@@ -4,29 +4,29 @@ import * as VueRouter from 'vue-router';
 import HeaderComponent from './header.component.vue';
 import AboutMeComponent from './about-me.component.vue';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faGithub)
-library.add(faLinkedin)
+library.add(faGithub);
+library.add(faLinkedin);
 
-const root = Vue.createApp({})
+const root = Vue.createApp({});
 
 const routes = [
   { path: '/', component: AboutMeComponent },
-]
+];
 
-root.component('header-component', HeaderComponent)
-root.component('font-awesome-icon', FontAwesomeIcon)
+root.component('header-component', HeaderComponent);
+root.component('font-awesome-icon', FontAwesomeIcon);
 
 const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes: routes
-})
+});
 
-root.use(router)
-const vm = root.mount('#app')
+root.use(router);
+const vm = root.mount('#app');
 
 
 
