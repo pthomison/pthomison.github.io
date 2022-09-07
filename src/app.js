67f3166a4b4ab6f4f58baafdf2b20ea213f16579
pthomison <p.thomison@gmail.com>
2022-09-07 10:05:43 -0700

    cleaning

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import * as Vue from 'Vue';
-import * as VueRouter from 'vue-router';
+import { createApp } from 'Vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import HeaderComponent from './header.component.vue';
 import AboutMeComponent from './about-me.component.vue';
@@ -11,7 +11,7 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 library.add(faGithub);
 library.add(faLinkedin);
 
-const root = Vue.createApp({});
+const root = createApp({});
 
 const routes = [
   { path: '/', component: AboutMeComponent },
@@ -20,13 +20,13 @@ const routes = [
 root.component('header-component', HeaderComponent);
 root.component('font-awesome-icon', FontAwesomeIcon);
 
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHashHistory(),
+const router = createRouter({
+  history: createWebHashHistory(),
   routes: routes
 });
 
 root.use(router);
-const vm = root.mount('#app');
+root.mount('#app');
 
 
 
